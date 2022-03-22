e8f76012eeb85a0c5da861780f3f856dd7a1e458
Bruno Vecchi <bno.vecchi@gmail.com>
2022-03-21 22:06:27 -0300

    end chapter 4

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,20 +1,27 @@
-import "./Expenses.css"
+import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
-    return (
-        <Card className="expenses">
-            <h2>Let's get started!</h2>
-            <ExpenseItem
-                {...props.items[0]} />
-            <ExpenseItem
-                {...props.items[1]} />
-            <ExpenseItem
-                {...props.items[2]} />
-            <ExpenseItem
-                {...props.items[3]} />
-        </Card>)
-}
+  const [year, setYear] = useState("");
+  const filterChangeHandler = (selectedYear) => {
+    setYear(selectedYear);
+    console.log(year);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={year} onChangeFilter={filterChangeHandler} />
+        <ExpenseItem {...props.items[0]} />
+        <ExpenseItem {...props.items[1]} />
+        <ExpenseItem {...props.items[2]} />
+        <ExpenseItem {...props.items[3]} />
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
