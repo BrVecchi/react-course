7a393dad592e4624f92185c56878adf79d155fba
Bruno Vecchi <bno.vecchi@gmail.com>
2022-03-24 13:43:41 -0300

    until lesson 67

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -5,20 +5,32 @@ import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 
 const Expenses = (props) => {
-  const [year, setYear] = useState("");
+  const [filteredYear, setFilteredYear] = useState("");
+
   const filterChangeHandler = (selectedYear) => {
-    setYear(selectedYear);
-    console.log(year);
+    setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p> No expenses found. </p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem key={expense.id} {...expense} />
+    ));
+  }
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter selected={year} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem {...props.items[0]} />
-        <ExpenseItem {...props.items[1]} />
-        <ExpenseItem {...props.items[2]} />
-        <ExpenseItem {...props.items[3]} />
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expensesContent}
       </Card>
     </div>
   );
